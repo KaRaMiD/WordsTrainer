a89590500f33e61f2fa2cb2cf19a24599d211438
k03440k <mixshanyagg@gmail.com>
2023-06-08 23:56:58 +0300

    std::vector optimization

--- WordsManager.cpp
+++ WordsManager.cpp
@@ -1,7 +1,7 @@
 #include "WordsManager.hpp"
 
 #include "WordsHandler.hpp"
-
+#include <iostream>
 namespace WordsTrainer
 {
     WordsManager::WordsManager(const std::string_view& wordsFileName)
@@ -9,6 +9,8 @@ namespace WordsTrainer
         const auto rwords = WordsHandler::GetRawWordsFromTheFile(wordsFileName);
 
         const auto count = std::count(rwords.begin(), rwords.end(), newLineSymbol);
+
+        m_Vocabulary.reserve(count);
 
         for (size_t i = 0; i < count; i++)
         {
